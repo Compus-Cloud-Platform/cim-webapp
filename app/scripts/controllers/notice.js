8f8fdc86fa81b3a2416c67bbbe1325f9f7bf9053
ruigao <ruigao2@ebay.com>
2015-05-10 00:50:37 +0800

    Change angular from 1.3 to 1.2

---
+++ app/scripts/controllers/notice.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name webappApp.controller:NoticeCtrl
+ * @description
+ * # NoticeCtrl
+ * Controller of the webappApp
+ */
+Site.controller('NoticeCtrl', ['$scope', 'AuthSrv', '$state', function ($scope, AuthSrv, $state) {
+  console.log("NoticeCtrl");
+
+  // left slide bar
+
+
+}]);
